dd8449e9d057a7d42dcae29436ca6b6b75a2295b
fernando-angeli <fernando_angeli@hotmail.com>
2022-07-22 15:44:11 -0300

    Database seeding

--- backend/src/main/java/com/example/dscatalog/entities/Category.java
+++ backend/src/main/java/com/example/dscatalog/entities/Category.java
@@ -15,7 +15,7 @@ import java.util.Objects;
 public class Category implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
